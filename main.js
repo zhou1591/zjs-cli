4ec01224726c4d4a4cec10c49cbdaf9f5b6d41f4
周靖松 <1402663440@qq.com>
2019-10-04 14:47:12 +0800

    'zjs-cli'

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ const chalk = require('chalk')
 const downGit = require('./src/downLoad')
 const options = require('./src/options')
 
+
 cmd.command('init').description('初始化模板').action(async (args) => {
-    console.log(chalk.yellow('zjs脚手架初始化模板，不要问我为什么是中文的，因为我英语不好 \n'))
     typeof args !== 'string' && (console.log(chalk.red('缺少必填参数')), process.exit(1))
+    console.log(chalk.yellow('zjs脚手架初始化模板，不要问我为什么是中文的，因为我英语不好 \n'))
     // 填选项
     await options()
     // 拉取
     await downGit(args)
 })
-
-cmd.parse(process.argv)
+cmd.parse(process.argv) 
